2d7267f43ccb9e45df98f198c954df1f00f22bdd
Pushpesh Pandey <pushpeshcool91@gmail.com>
2026-04-29 15:37:03 +0530

    Update findAll func

--- Spy Mission/cpp/backtracking.cpp
+++ Spy Mission/cpp/backtracking.cpp
@@ -4,22 +4,36 @@
 
 using namespace std;
 
+static bool nodeIsBlocked(int nodeId, bool blockedList[]) {
+    return blockedList[nodeId] == true;
+}
+
+static bool nodeAlreadyVisited(int nodeId, bool visitedList[]) {
+    return visitedList[nodeId] == true;
+}
+
 void findAllPaths(int current, int end, bool visited[], bool blocked[], vector<int>& path) {
-    if(blocked[current]) return;
+    if (nodeIsBlocked(current, blocked) == true) return;
 
     visited[current] = true;
     path.push_back(current);
 
-    if(current == end) {
+    if (current == end) {
         cout << "Path: ";
-        for(int x : path)
-            cout << x << " ";
+        int printIdx = 0;
+        while (printIdx < (int)path.size()) {
+            cout << path[printIdx] << " ";
+            printIdx = printIdx + 1;
+        }
         cout << endl;
-    }
-    else {
-        for(auto edge : graph[current]) {
-            if(!visited[edge.dest_node])
-                findAllPaths(edge.dest_node, end, visited, blocked, path);
+    } else {
+        int edgeIdx = 0;
+        while (edgeIdx < (int)graph[current].size()) {
+            int neighborNode = graph[current][edgeIdx].dest_node;
+            if (nodeAlreadyVisited(neighborNode, visited) == false) {
+                findAllPaths(neighborNode, end, visited, blocked, path);
+            }
+            edgeIdx = edgeIdx + 1;
         }
     }
 
