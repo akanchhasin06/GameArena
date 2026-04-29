08aed4585f3363fae2502afbb428b849f9fdf0e5
Pushpesh Pandey <pushpeshcool91@gmail.com>
2026-04-29 15:36:12 +0530

    Minor code clarity

--- Puzzle Solver/cpp/branchBound.cpp
+++ Puzzle Solver/cpp/branchBound.cpp
@@ -6,118 +6,182 @@
 #include <map>
 using namespace std;
 
-const int goal[9] = {1,2,3,4,5,6,7,8,0};
+const int goalPattern[9] = {1, 2, 3, 4, 5, 6, 7, 8, 0};
 
 static int stateSnapshots[1000][9];
 static int movesList[1000];
-static int movesCount, statesExpanded, pathCost;
+static int movesCount;
+static int statesExpanded;
+static int pathCost;
 
-int lowerBound(const int s[9]) {
-    int dist = 0;
-    for(int i = 0; i < 9; i++) {
-        if(s[i] == 0) continue;
-        int val = s[i] - 1;
-        dist += abs(i/3 - val/3) + abs(i%3 - val%3);
+static int computeLowerBound(const int boardState[9]) {
+    int totalDistance = 0;
+    int tileIdx = 0;
+    while (tileIdx < 9) {
+        if (boardState[tileIdx] != 0) {
+            int targetPosition = boardState[tileIdx] - 1;
+            int targetRow = targetPosition / 3;
+            int currentRow = tileIdx / 3;
+            int targetCol = targetPosition % 3;
+            int currentCol = tileIdx % 3;
+            int rowDistance = abs(currentRow - targetRow);
+            int colDistance = abs(currentCol - targetCol);
+            totalDistance = totalDistance + rowDistance + colDistance;
+        }
+        tileIdx = tileIdx + 1;
     }
-    return dist;
+    return totalDistance;
 }
 
-bool isGoal(const int s[9]) {
-    for(int i = 0; i < 9; i++)
-        if(s[i] != goal[i]) return false;
+static bool boardMatchesGoal(const int boardState[9]) {
+    int idx = 0;
+    while (idx < 9) {
+        if (boardState[idx] != goalPattern[idx]) return false;
+        idx = idx + 1;
+    }
     return true;
 }
 
-string toKey(const int s[9]) {
-    string k;
-    for(int i = 0; i < 9; i++) k += char(s[i] + '0');
-    return k;
+static string encodeBoard(const int boardState[9]) {
+    string encoded;
+    int idx = 0;
+    while (idx < 9) {
+        encoded = encoded + char(boardState[idx] + '0');
+        idx = idx + 1;
+    }
+    return encoded;
 }
 
-int findZero(const int s[9]) {
-    for(int i = 0; i < 9; i++)
-        if(s[i] == 0) return i;
+static int findBlankPosition(const int boardState[9]) {
+    int idx = 0;
+    while (idx < 9) {
+        if (boardState[idx] == 0) return idx;
+        idx = idx + 1;
+    }
     return -1;
 }
 
-bool solvable(const int s[9]) {
-    int inv = 0;
-    for(int i = 0; i < 9; i++)
-        for(int j = i+1; j < 9; j++)
-            if(s[i] && s[j] && s[i] > s[j]) inv++;
-    return inv % 2 == 0;
+static bool inversionsEven(const int boardState[9]) {
+    int invCount = 0;
+    int outer = 0;
+    while (outer < 9) {
+        int inner = outer + 1;
+        while (inner < 9) {
+            bool bothNonZero = boardState[outer] != 0 && boardState[inner] != 0;
+            if (bothNonZero == true && boardState[outer] > boardState[inner]) {
+                invCount = invCount + 1;
+            }
+            inner = inner + 1;
+        }
+        outer = outer + 1;
+    }
+    return invCount % 2 == 0;
 }
 
-struct Node {
-    array<int,9> state;
-    int g, bound, blank;
-    vector<int> moves;
-    vector<array<int,9>> path;
+struct BoundNode {
+    array<int, 9> boardTiles;
+    int gCost;
+    int boundEstimate;
+    int blankPos;
+    vector<int> moveHistory;
+    vector<array<int, 9>> boardHistory;
 
-    int f() const { return g + bound; }
-    bool operator>(const Node& o) const { return f() > o.f(); }
+    int totalCost() const { return gCost + boundEstimate; }
+    bool operator>(const BoundNode& other) const { return totalCost() > other.totalCost(); }
 };
 
 extern "C" {
 
 EMSCRIPTEN_KEEPALIVE
-int runBranchBound(int s0,int s1,int s2,int s3,int s4,int s5,int s6,int s7,int s8) {
-    int startArr[9] = {s0,s1,s2,s3,s4,s5,s6,s7,s8};
-    if(!solvable(startArr)) return -1;
+int runBranchBound(int s0, int s1, int s2, int s3, int s4, int s5, int s6, int s7, int s8) {
+    int initialTiles[9] = {s0, s1, s2, s3, s4, s5, s6, s7, s8};
 
-    movesCount = statesExpanded = pathCost = 0;
+    if (inversionsEven(initialTiles) == false) return -1;
 
-    Node start;
-    for(int i = 0; i < 9; i++) start.state[i] = startArr[i];
-    start.g = 0;
-    start.bound = lowerBound(start.state.data());
-    start.blank = findZero(start.state.data());
-    start.path.push_back(start.state);
+    movesCount = 0;
+    statesExpanded = 0;
+    pathCost = 0;
 
-    priority_queue<Node, vector<Node>, greater<Node>> pq;
-    pq.push(start);
+    BoundNode rootNode;
+    int fi = 0;
+    while (fi < 9) {
+        rootNode.boardTiles[fi] = initialTiles[fi];
+        fi = fi + 1;
+    }
+    rootNode.gCost = 0;
+    rootNode.boundEstimate = computeLowerBound(rootNode.boardTiles.data());
+    rootNode.blankPos = findBlankPosition(rootNode.boardTiles.data());
+    rootNode.boardHistory.push_back(rootNode.boardTiles);
 
-    map<string,int> best;
-    int dir[] = {-3, 3, -1, 1};
+    priority_queue<BoundNode, vector<BoundNode>, greater<BoundNode>> expansionQueue;
+    expansionQueue.push(rootNode);
 
-    while(!pq.empty()) {
-        Node cur = pq.top(); pq.pop();
-        statesExpanded++;
+    map<string, int> bestKnownCost;
+    int movementOffsets[4] = {-3, 3, -1, 1};
 
-        if(isGoal(cur.state.data())) {
-            pathCost  = cur.g;
-            movesCount = (int)cur.moves.size();
+    while (expansionQueue.empty() == false) {
+        BoundNode currentNode = expansionQueue.top();
+        expansionQueue.pop();
+        statesExpanded = statesExpanded + 1;
 
-            for(int i = 0; i < movesCount && i < 1000; i++) {
-                movesList[i] = cur.moves[i];
-                for(int j = 0; j < 9; j++)
-                    stateSnapshots[i][j] = cur.path[i+1][j];
+        if (boardMatchesGoal(currentNode.boardTiles.data()) == true) {
+            pathCost = currentNode.gCost;
+            movesCount = (int)currentNode.moveHistory.size();
+
+            int stepIdx = 0;
+            while (stepIdx < movesCount && stepIdx < 1000) {
+                movesList[stepIdx] = currentNode.moveHistory[stepIdx];
+                int cellIdx = 0;
+                while (cellIdx < 9) {
+                    stateSnapshots[stepIdx][cellIdx] = currentNode.boardHistory[stepIdx + 1][cellIdx];
+                    cellIdx = cellIdx + 1;
+                }
+                stepIdx = stepIdx + 1;
             }
             return movesCount;
         }
 
-        string key = toKey(cur.state.data());
-        if(best.count(key) && best[key] <= cur.g) continue;
-        best[key] = cur.g;
+        string currentKey = encodeBoard(currentNode.boardTiles.data());
+        bool alreadyBetter = bestKnownCost.count(currentKey) > 0 && bestKnownCost[currentKey] <= currentNode.gCost;
+        if (alreadyBetter == true) continue;
+        bestKnownCost[currentKey] = currentNode.gCost;
 
-        int b = cur.blank;
-        for(int d = 0; d < 4; d++) {
-            int nb = b + dir[d];
-            if(nb < 0 || nb > 8) continue;
-            if(d == 2 && b % 3 == 0) continue;
-            if(d == 3 && b % 3 == 2) continue;
+        int blankPosition = currentNode.blankPos;
 
-            Node nxt = cur;
-            swap(nxt.state[b], nxt.state[nb]);
-            nxt.blank = nb;
-            nxt.g = cur.g + 1;
-            nxt.bound = lowerBound(nxt.state.data());
-            nxt.moves.push_back(nxt.state[b]);
-            nxt.path.push_back(nxt.state);
+        int dirIdx = 0;
+        while (dirIdx < 4) {
+            int newBlankPos = blankPosition + movementOffsets[dirIdx];
 
-            string k = toKey(nxt.state.data());
-            if(!best.count(k) || best[k] > nxt.g)
-                pq.push(nxt);
+            if (newBlankPos < 0 || newBlankPos > 8) {
+                dirIdx = dirIdx + 1;
+                continue;
+            }
+            if (dirIdx == 2 && blankPosition % 3 == 0) {
+                dirIdx = dirIdx + 1;
+                continue;
+            }
+            if (dirIdx == 3 && blankPosition % 3 == 2) {
+                dirIdx = dirIdx + 1;
+                continue;
+            }
+
+            BoundNode childNode = currentNode;
+            int tempValue = childNode.boardTiles[blankPosition];
+            childNode.boardTiles[blankPosition] = childNode.boardTiles[newBlankPos];
+            childNode.boardTiles[newBlankPos] = tempValue;
+            childNode.blankPos = newBlankPos;
+            childNode.gCost = currentNode.gCost + 1;
+            childNode.boundEstimate = computeLowerBound(childNode.boardTiles.data());
+            childNode.moveHistory.push_back(childNode.boardTiles[blankPosition]);
+            childNode.boardHistory.push_back(childNode.boardTiles);
+
+            string childKey = encodeBoard(childNode.boardTiles.data());
+            bool childHasBetter = bestKnownCost.count(childKey) > 0 && bestKnownCost[childKey] <= childNode.gCost;
+            if (childHasBetter == false) {
+                expansionQueue.push(childNode);
+            }
+
+            dirIdx = dirIdx + 1;
         }
     }
     return 0;
